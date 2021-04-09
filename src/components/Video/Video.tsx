86ad9eabadd6cfe02f5f7f02a211a39d3846ff8d
ArturRusak <arturrusak@gmail.com>
2021-04-09 20:36:14 +0300

    implementing Video component

---
+++ src/components/Video/Video.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type VideoProps = {
+  autoPlay?: boolean;
+  src: string;
+  style?: React.CSSProperties,
+  type: string;
+  width?: string;
+  height?: string;
+};
+
+export const Video: React.FC<VideoProps> = (props) => {
+  const { autoPlay = false, type, src, ...rest } = props;
+  console.log(autoPlay);
+  return <>
+    <h1>Video</h1>
+    <video
+      width="100%"
+      height="100%"
+      autoPlay={autoPlay}
+      {...rest}
+    >
+      <source
+        src={src}
+        type={type}
+      />
+    </video>
+  </>
+};

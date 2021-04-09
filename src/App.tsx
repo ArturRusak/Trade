97c6f504e0b33454ab719582336714cc61f092d5
ArturRusak <arturrusak@gmail.com>
2021-04-09 19:36:10 +0300

    init config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Home } from "./screens/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Header</h2>
+      </header>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;

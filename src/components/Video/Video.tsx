5cae53d048bf3540be118845a96985d1416d12c9
ArturRusak <arturrusak@gmail.com>
2021-04-10 01:10:47 +0300

    refactoring

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -12,9 +12,7 @@ type VideoProps = {
 
 export const Video: React.FC<VideoProps> = (props) => {
   const { autoPlay = false, type, src, ...rest } = props;
-  console.log(autoPlay);
   return <>
-    <h1>Video</h1>
     <video
       width="100%"
       height="100%"
